3ad95b5272dc5c748225edb289fa8d7cff287a6e
luca <lucacambi77@gmail.com>
2019-11-30 22:27:41 +0100

    mockito added

--- src/test/java/it/cambi/superawesome/test/AnagramsTest.java
+++ src/test/java/it/cambi/superawesome/test/AnagramsTest.java
@@ -3,10 +3,11 @@
  */
 package it.cambi.superawesome.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,41 +17,34 @@ import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
  * @author luca
  *
  */
 @TestMethodOrder(OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class)
 public class AnagramsTest
 {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-    private final PrintStream originalErr = System.err;
+    private PrintStream out;
 
     @BeforeEach
     public void setUpStreams()
     {
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-    }
-
-    @AfterEach
-    public void restoreStreams()
-    {
-        System.setOut(originalOut);
-        System.setErr(originalErr);
+        out = mock(PrintStream.class);
+        System.setOut(out);
     }
 
     @ParameterizedTest
@@ -73,14 +67,19 @@ public class AnagramsTest
 
         buf.close();
 
-        assertEquals("abc\n" + "fun\n" + "bac\n" + "fun\n" + "cba\n" + "unf\n" + "hello\n", outContent.toString());
+        verify(out).println("abc");
+        verify(out, times(2)).println("fun");
+        verify(out).println("bac");
+        verify(out).println("unf");
+        verify(out).println("hello");
+        verify(out).println("cba");
 
     }
 
     private HashMap<BigInteger, TreeSet<String>> resultMap = new HashMap<>();
 
     @SuppressWarnings("serial")
-    Map<Character, Integer> alphabetMap = new HashMap<Character, Integer>()
+    private Map<Character, Integer> alphabetMap = new HashMap<Character, Integer>()
     {
         {
 
@@ -115,7 +114,7 @@ public class AnagramsTest
     };
 
     @ParameterizedTest
-    @ValueSource(strings = { "src/main/resources/task/data/example1.txt", "src/main/resources/task/data/example2.txt" })
+    @ValueSource(strings = { "src/main/resources/task/data/example1.txt" })
     @Order(2)
     public void testAnagrams2(String file) throws IOException
     {
@@ -125,7 +124,7 @@ public class AnagramsTest
 
         String line = buf.readLine();
 
-        checkLine(line, alphabetMap);
+        checkLine(line);
 
         int currentLength = line.length();
 
@@ -138,7 +137,7 @@ public class AnagramsTest
                 resultMap = new HashMap<>();
             }
 
-            checkLine(line, alphabetMap);
+            checkLine(line);
 
             line = buf.readLine();
 
@@ -148,25 +147,27 @@ public class AnagramsTest
 
         buf.close();
 
+        verify(out).println("fun,unf");
+        verify(out).println("abc,bac,cba");
+        verify(out).println("hello");
     }
 
     /**
      * @param line
-     * @param map
      * @return
      */
-    private void checkLine(String line, Map<Character, Integer> map)
+    private void checkLine(String line)
     {
-        BigInteger value = new BigInteger("1");
+        AtomicReference<BigInteger> valueHolder = new AtomicReference<>();
+        valueHolder.set(new BigInteger("1"));
 
-        char[] array = line.toCharArray();
+        line.chars().forEach(c -> {
+            valueHolder.getAndAccumulate(BigInteger.valueOf(alphabetMap.get((char) c)), (previous, x) -> previous.multiply(x));
+        });
 
-        for (int i = 0; i < array.length; i++)
-            value = value.multiply(BigInteger.valueOf(map.get(line.charAt(i))));
-
-        TreeSet<String> set = resultMap.getOrDefault(value, new TreeSet<String>());
+        TreeSet<String> set = resultMap.getOrDefault(valueHolder.get(), new TreeSet<String>());
         set.add(line);
-        resultMap.put(value, set);
+        resultMap.put(valueHolder.get(), set);
     }
 
     /**
