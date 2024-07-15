d40b46ec05f55277f3d23ef5b54697773edbaa64
luca <lucacambi77@gmail.com>
2024-07-15 17:42:33 +0200

    fix: clean

--- src/main/java/it/cambi/superawesome/domain/Errors.java
+++ src/main/java/it/cambi/superawesome/domain/Errors.java
@@ -14,8 +14,4 @@ public final class Errors
 
     public static final String ERR_FILE_NOT_EXISTS = "File does not exists";
 
-    public static final String ERR_INPUT_NOT_EMPTY = "Input must be not empty!";
-
-    public static final String ERR_INPUT_WORD_ATLEAST_TWO_LETTER = "Word must be at least of two letters!";
-
 }
